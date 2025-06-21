ae8fee66420f561e849001641484abccbd15f1dd
JaimeProyecto <Jaime.ubarne30@gmail.com>
2025-06-21 17:47:05 -0500

    correccion

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-    # login
-    path("admin/", admin.site.urls),
-    path("", include("core.urls")),
-]
