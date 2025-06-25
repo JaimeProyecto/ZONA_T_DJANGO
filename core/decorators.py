5655ce4985be8947260f651abcd633b9bceba540
JaimeProyecto <Jaime.ubarne30@gmail.com>
2025-06-25 16:42:02 -0500

    ajuste de los reportes e inicio

--- core/decorators.py
+++ core/decorators.py
@@ -6,7 +6,7 @@ def es_vendedor(user):
     Devuelve True si el usuario está autenticado y tiene el rol 'vendedor'.
     Ajusta 'vendedor' al nombre exacto que uses en tu tabla de roles.
     """
-    return user.is_authenticated and user.roles.filter(name="vendedor").exists()
+    return user.is_authenticated and user.is_superuser.filter(name="vendedor").exists()
 
 
 def es_admin(user):
@@ -14,4 +14,4 @@ def es_admin(user):
     Devuelve True si el usuario está autenticado y tiene el rol 'admin'.
     Ajusta 'admin' al nombre exacto que uses en tu tabla de roles.
     """
-    return user.is_authenticated and user.roles.filter(name="admin").exists()
+    return user.is_authenticated and user.is_superuser.filter(name="admin").exists()
