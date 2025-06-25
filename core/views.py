0d86c0c3e41e08c372db59e1a5c9734266d19c9f
JaimeProyecto <Jaime.ubarne30@gmail.com>
2025-06-25 17:07:02 -0500

    ajuste de los reportes e inicio

--- core/views.py
+++ core/views.py
@@ -10,7 +10,16 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.forms import AuthenticationForm
 from django.db import transaction
-from django.db.models import Sum, F, Q, Value, ExpressionWrapper, DecimalField, Count
+from django.db.models import (
+    Sum,
+    F,
+    Q,
+    Value,
+    ExpressionWrapper,
+    DecimalField,
+    Count,
+    FloatField,
+)
 from django.db.models.functions import Coalesce
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
